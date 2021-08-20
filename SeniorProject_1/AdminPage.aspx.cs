7fa6cc5506901c4ff339a1e284d0da6d51272918
Sebastian Lara <selara@email.neit.edu>
2021-08-19 20:34:50 -0400

    Added Student username/password as part of the registration. Added edit warning events

--- SeniorProject_1/AdminPage.aspx.cs
+++ SeniorProject_1/AdminPage.aspx.cs
@@ -14,7 +14,10 @@ namespace SeniorProject_1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!this.IsPostBack)
+            {
+                this.DataBind();
+            }
         }
 
         protected void Butsubmit_Click(object sender, EventArgs e)
@@ -35,6 +38,7 @@ namespace SeniorProject_1
                 GdTutor.Visible = false;
                 GdCourse.DataSource = dt;
                 GdCourse.DataBind();
+                
 
             }
             else if (DDL1.SelectedItem.Text == "student1")
@@ -95,6 +99,7 @@ namespace SeniorProject_1
 
         protected void GdCourse_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
+            
             int Cid = (int)GdCourse.DataKeys[e.RowIndex].Values[0];
 
             string mainconn = ConfigurationManager.ConnectionStrings["se425_teamblue-ConnectionString"].ConnectionString;
@@ -203,9 +208,8 @@ namespace SeniorProject_1
             sqlcomm.Parameters.AddWithValue("@Tqualification", Tqualification);
            
             sqlcomm.ExecuteNonQuery();
-
-
             sqlconn.Close();
+            GdTutor.DataBind();
         }
 
         protected void GdTutor_RowDeleting(object sender, GridViewDeleteEventArgs e)
@@ -220,11 +224,23 @@ namespace SeniorProject_1
             sqlcomm.Parameters.AddWithValue("@id", id);
             sqlcomm.ExecuteNonQuery();
             sqlconn.Close();
+            
+            GdTutor.DataBind();
+          
+
         }
 
         protected void GdTutor_RowEditing(object sender, GridViewEditEventArgs e)
         {
             GdTutor.EditIndex = e.NewEditIndex;
+        }
+
+        protected void GdTutor_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GdTutor.EditIndex)
+            {
+                (e.Row.Cells[0].Controls[0] as LinkButton).Attributes["onclick"] = "return confirm('Do you want to edit this row?');";
+            }
         }
     }
 }
--- SeniorProject_1/StudentPage.aspx.designer.cs
+++ SeniorProject_1/StudentPage.aspx.designer.cs
@@ -123,6 +123,24 @@ namespace SeniorProject_1
         protected global::System.Web.UI.WebControls.Literal LitJoinDate;
 
         /// <summary>
+        /// TxtSUsername control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtSUsername;
+
+        /// <summary>
+        /// TxtSPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtSPassword;
+
+        /// <summary>
         /// ButStudent control.
         /// </summary>
         /// <remarks>
